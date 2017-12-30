086e678aade8f3d02228eadb7e7492b61e8be238
Dipika Chadha <dipika.chadha@gmail.com>
2017-12-30 18:59:16 -0500

    Sprinkle comments, minor rearrangements

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ function Cat () {
   this.name = ko.observable('Tabby');
   this.clickCount = ko.observable(0);
   this.imgSrc = ko.observable('img/22252709_010df3379e_z.jpg');
+  this.nickNames = ko.observableArray(['Timmy', 'Tabbu', 'Tiggy']);
 
   this.catLevel = ko.computed (_ => {
     const clicks = this.clickCount();
@@ -12,14 +13,21 @@ function Cat () {
       default: return 'Error';
     };
   }, this);
-
-  this.nickNames = ko.observableArray(['Timmy', 'Tabbu', 'Tiggy']);
 }
 
 function AppViewModel () {
   const that = this; // save ViewModel context for use later on
+
+  // We make a currentCat KO observable object from raw cat data. The data
+  // differs from the returned object since it has all it's properties as KO
+  // observables and has added KO functions like catLevel.
   this.currentCat = ko.observable(new Cat());
-  this.incrementCounter = () => {
+
+  this.incrementCounter = _ => {
+    // The HTML uses with: currentCat context, and calls incrementCounter in
+    // that context, albeit with $parent. Hence, we need to act as per the
+    // currentCat context, or explicitly specify the scope we are referring to.
+    // I chose the latter with the statement below.
     that.currentCat().clickCount(that.currentCat().clickCount() + 1);
   };
 }
